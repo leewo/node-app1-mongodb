117391c53743939b66c03157e1a46074b07137df
wy <xiph@live.com>
2024-08-15 22:22:41 +0900

    1. 컨트롤러 분리 완료
    2. 입력 유효성 검사
    - 잘못된 데이터 차단: 서버 측에서 잘못된 데이터를 사전에 필터링한다
    - 보안 강화: 악의적인 입력을 방지한다
    - 일관성 유지: 모든 요청에 대해 일관된 유효성 검사를 적용할 수 있다
    - 비밀번호 정책 강화: 비밀번호 복잡성 요구사항을 추가하여 보안을 강화할 수 있다

--- routes/v1/userRoutes.mjs
+++ routes/v1/userRoutes.mjs
@@ -1,9 +1,10 @@
 import { register, login } from '../../controllers/userController.mjs';
+import { registerValidator, validate } from '../../middleware/validators.mjs';
 import { Router } from 'express'; // ES 모듈 문법을 사용하여 express 패키지에서 Router를 직접 import
                                   // 중괄호 { } 를 사용하는 것은 named import를 의미하며, express 모듈에서 Router라는 이름의 export를 가져온다
 const router = Router();          // import한 Router 함수를 호출하여 새로운 router 인스턴스를 생성. 이 부분은 CommonJS 문법과 동일
 
-router.post('/register', register);
+router.post('/register', registerValidator, validate, register);
 router.post('/login', login);
 
 export default router;
