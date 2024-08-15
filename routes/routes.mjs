c408fa2812ecc9bee82270287faf4a07332534df
wy <xiph@live.com>
2024-08-15 20:21:55 +0900

    1. password 암호화
    2. login api 추가

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -1,22 +1,44 @@
 import User from '../models/user.mjs';
+import bcrypt from 'bcrypt';
 
 // const router = require('express').Router;
 import { Router } from 'express'; // ES 모듈 문법을 사용하여 express 패키지에서 Router를 직접 import
-                                  // 중괄호 { } 를 사용하는 것은 named import를 의미하며, express 모듈에서 Router라는 이름의 export를 가져온다
+// 중괄호 { } 를 사용하는 것은 named import를 의미하며, express 모듈에서 Router라는 이름의 export를 가져온다
 const router = Router();          // import한 Router 함수를 호출하여 새로운 router 인스턴스를 생성. 이 부분은 CommonJS 문법과 동일
 
 router.post('/register', async (req, res) => {
   try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(req.body, hashedPassword);
+
     const user = new User({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password: hashedPassword
     });
 
     await user.save();
     res.status(201).json({ message: 'User registered successfully', userId: user._id });
   } catch (error) {
     res.status(400).json({ message: 'Error registering user', error: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    res.status(200).json({ message: 'User logged in successfully' });
+  } catch (error) {
+    res.status(400).json({ message: 'Error logging in', error: error.message });
   }
 });
 
