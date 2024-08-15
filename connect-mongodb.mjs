6a8c35a2f2ea7ab693d44568c0214c976f5a40eb
wy <xiph@live.com>
2024-08-15 21:03:40 +0900

    주석추가

--- connect-mongodb.mjs
+++ connect-mongodb.mjs
@@ -9,7 +9,7 @@ async function connectToMongoDB() {
 
   async function connectToMongoDB() {
     try {
-      const uri = process.env.MONGODB_URI.replace('${MONGODB_PASSWORD}', process.env.MONGODB_PASSWORD);
+      const uri = process.env.MONGODB_URI.replace('${MONGODB_PASSWORD}', process.env.MONGODB_PASSWORD); // MONGODB_URI에서 ${MONGODB_PASSWORD} 플레이스홀더를 사용. 이는 실제 비밀번호로 대체
       const clientOptions = {
         serverApi: { version: '1', strict: true, deprecationErrors: true }
       };
